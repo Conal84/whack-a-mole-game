110ed0977664c9ae70fe684806eb6a62c9a67040
Conal Walsh <conal.walsh1@gmail.com>
2021-03-02 20:16:43 +0000

    Added game start and bonkMole functions and tested

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -17,3 +19,30 @@ function randomHole(holes) {
   lastHole = hole;
   return hole;
 }
+
+function popMole() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) popMole();
+  }, time);
+}
+
+function bonkMole(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.classList.remove('up');
+  scoreBoard.textContent = score;
+}
+
+function startGame() {
+  scoreBoard.textContent = '0';
+  timeUp = false;
+  score = 0;
+  popMole();
+  setTimeout(() => (timeUp = true), 10000);
+}
+
+moles.forEach((mole) => mole.addEventListener('click', bonkMole));
